34724c3ae4ecd61014ed7b5a802a97fe73b5f13d
songyihong <songyihong1@163.com>
2013-03-11 21:07:12 +0800

    seajs配置

---
+++ config.js
@@ -0,0 +1,21 @@
+seajs.config({
+    //加载seajs插件
+    plugins: ['shim','text','nocache'],
+    // 别名配置
+    alias: {
+        'jquery': {
+            src: 'jquery-1.9.1.min.js',
+            exports: 'jQuery'
+        }
+    },
+    //设置路径
+    paths:{
+       
+    },
+    // 调试模式
+    debug: true,
+    // 文件编码
+    charset: 'utf-8'
+});
+
+
